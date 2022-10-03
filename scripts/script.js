02eb130f2634962eec3a6f1d99f7076cb6634615
ZecaFiles <brunacamera2@gmail.com>
2022-10-03 16:53:54 -0300

    update files

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 
-TimerConfiguration()
 
 function TimerConfiguration() {
-  const title = document.getElementById('#titulo');
 
-// ✅ Change (replace) the text of the element
-  //title.innerText = 'New text';
+  var teste = document.getElementById("#content")
+  teste.innerHTML = "çaljfdasjfçaljfdldjsf"
+
+  // ✅ Change (replace) the text of the element
+  title.innerText = 'New text';
 
 }
+function change_text(){
+  document.getElementById("demo").innerHTML = "VZCVX2";
+  }
